2ace2d4869fccfd4a505960f12451927741be511
jz423 <jz423@duke.edu>
2023-01-26 13:59:46 -0500

    5 blank page create

--- docker-deploy/web-app/Ride_Sharing_Service/base/urls.py
+++ docker-deploy/web-app/Ride_Sharing_Service/base/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('logIn/',views.logIn, name='logIn'),
+    path('logOut/',views.logOut, name='logOut'),
+    path('profile/',views.profile, name='profile'),
+    path('createAccount/',views.createAccount, name='createAccount'),
 ]
--- docker-deploy/web-app/Ride_Sharing_Service/base/views.py
+++ docker-deploy/web-app/Ride_Sharing_Service/base/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, world. You're at the home.")
+def logIn(request):
+    return HttpResponse("This is Log in page")
+def logOut(request):
+    return HttpResponse("This is Log out page")
+def profile(request):
+    return HttpResponse("This is profile page")
+def createAccount(request):
+    return HttpResponse("This is create account page")
