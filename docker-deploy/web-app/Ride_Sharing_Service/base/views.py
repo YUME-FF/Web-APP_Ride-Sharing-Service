0e5b0cc00f6409f59e9ce76553a2118351d688f4
jz423 <jz423@duke.edu>
2023-01-27 15:02:05 -0500

    lgoin logout finish

--- docker-deploy/web-app/Ride_Sharing_Service/base/views.py
+++ docker-deploy/web-app/Ride_Sharing_Service/base/views.py
@@ -1,27 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth import login as django_login
+from django.contrib.auth import logout as django_logout
+
+from django.contrib.auth import authenticate, logout, login
 rooms = [
     {'id':1, 'name':'Lets python'},
     {'id':2, 'name':'Lets Django'},
 ]
 
 def home(request):
-    return render(request, 'base/home.html', {'rooms':rooms})
+    return render(request, 'base/home.html', {'rooms':rooms}) 
+
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         
-        try:
-            user = User.objects.get(username = username)
-        except:
-            messages.error(request,'User does not exist!')
+        user = authenticate(request, username=username, password = password)
+        if user is not None:
+            django_login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or passowrd is not correct')
     return render(request, 'base/login.html')
+
 def logout(request):
-    return render(request, 'base/logout.html')
+    django_logout(request)
+    return redirect('home')
+
+
 def profile(request):
     return render(request, 'base/profile.html')
 def createAccount(request):
