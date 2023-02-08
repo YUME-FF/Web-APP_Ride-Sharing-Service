4edc99cb4c8405286abf25a9df19c6ed547f59f3
Zhe Fan <zf70@duke.edu>
2023-02-08 12:27:54 -0500

    Update settings.py

--- docker-deploy/web-app/Ride_Sharing_Service/settings.py
+++ docker-deploy/web-app/Ride_Sharing_Service/settings.py
@@ -136,8 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #gmail_send/settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zjy1298892386@gmail.com'
-EMAIL_HOST_PASSWORD = 'gqpfufmssoxnyvsv' #past the key or password app here
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
